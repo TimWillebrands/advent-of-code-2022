dc98d54f0fd36dc88202cb1badc768136af2f0b0
Tim Willebrands <mail@timwillebrands.nl>
2022-12-11 14:39:30 +0100

    dumbdum

--- 11/part2.ts
+++ 11/part2.ts
@@ -7,24 +7,29 @@ const monkeys = inputFull
   .map((monkey) => ({
     name: monkey[0].slice(0, -1),
     startItems: monkey[1].slice(18).split(", ").map(Number),
-    operation: (old: number) =>
-      eval(monkey[2].slice(19).replaceAll("old", old.toString())) as number,
-    target: (item: number) =>
-      (item % Number(monkey[3].slice(21))) === 0
+    operation: (old: bigint) =>
+      eval(
+        monkey[2]
+          .slice(19)
+          .replace(/\d+/g, (d) => `${d}n`)
+          .replaceAll("old", `${old.toString()}n`),
+      ) as bigint,
+    target: (item: bigint) =>
+      (item % BigInt(monkey[3].slice(21))) === BigInt(0)
         ? Number(monkey[4].split("monkey ")[1])
         : Number(monkey[5].split("monkey ")[1]),
   }));
 
-const monkeyItems = monkeys.map((monkey) => monkey.startItems);
+const monkeyItems = monkeys.map((monkey) => monkey.startItems.map(BigInt));
 const monkeyInspections = new Array<number>(monkeys.length).fill(0);
 
-for (let round = 0; round < 20; round++) {
+for (let round = 0; round < 10000; round++) {
   for (let m = 0; m < monkeys.length; m++) {
     const monkey = monkeys[m];
     const items = monkeyItems[m];
 
     while (items.length > 0) {
-      const item = Math.floor(monkey.operation(items.shift()!) / 3);
+      const item = monkey.operation(items.shift()!);
       const target = monkey.target(item);
       monkeyItems[target].push(item);
       monkeyInspections[m]++;
